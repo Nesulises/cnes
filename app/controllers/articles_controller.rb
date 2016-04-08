775110bd278ab74c4866f8e84930808cb2d6cdcd
Nesulises <nesulises777@gmail.com>
2016-04-07 23:40:54 -0600

    Commit pt 10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,5 +10,17 @@ class ArticlesController < ApplicationController
 		@articles = Article.find(params[:id])
 	end	
 	#GET /articles/new 
-
+	def new
+		#
+		@article = Article.new
+	end
+	#POST /articles 
+	def create
+		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
-class Article < ActiveRecord::Base
+  class Article < ActiveRecord::Base
+  	# Esto es lo que se genero deesde la consola 
+  	# Se puede decir que es la base de datos de Rails
+  	# No se le dijo el nombre de la tabla va a toma
+  	# el que se le dio a la clase en la terminal
+  	# y tomo el nombre y lo pluraliso o le agrego la "S"
+  	# y los campos los va agenara el Active Record
+  	validates :title, presence: true, uniqueness: true
+  	validates :body, presence: true, length: { minimum: 10 }
 end
