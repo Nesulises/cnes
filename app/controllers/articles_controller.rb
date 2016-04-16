a112d953d424ee99eb89af27a3471a2e7effdb2c
Nesulises <nesulises777@gmail.com>
2016-04-16 14:22:01 -0600

    17 se refactorizo el codigo y se agrego el contador de visitas

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,20 @@
 class ArticlesController < ApplicationController
+	#before_action :validate_user, except: [:show,:index]
 	#A ESTA RUTA SE ACCEDE CON: GET /articles
+	before_action :authenticate_user!, except: [:show,:index]
+	before_action :set_article, except: [:index,:new,:create] 
 	def index 
 		#Ob tiene todos los rgistros de la tabla Articles..
 		@articles = Article.all
 	end
 	#GET /articles/:id
 	def show 
-		#Encuentra un registros segun el ID
-		@article = Article.find(params[:id])
+		@article.update_visits_count
+		#Encuen tra un registros segun el ID
+		#@article = Article.find(params[:id])
 	end	
 	#GET /articles/new 
 	def new
-		#
 		@article = Article.new
 	end
 
@@ -31,13 +34,13 @@ class ArticlesController < ApplicationController
 		end
 	end
 	def destroy
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 	end	
 	#PUT /articles/
 	def update
-	  	@article = Article.find(params[:id])
+	  	#@article = Article.find(params[:id])
 	  	if  @article.update(article_params)
 	  		redirect_to @article
 	  	else
@@ -45,6 +48,12 @@ class ArticlesController < ApplicationController
 	  	end
 	end 
 	private
+	#def validate_user
+		#redirect_to new_user_session_path, notice: "Para agregar un articulo necesitas Iniciar Sesion"
+	#end
+	def set_article
+		@article = Article.find(params[:id])
+	end
 	def article_params
 		params.require(:article).permit(:title,:body)
 	end
--- app/models/article.rb
+++ app/models/article.rb
@@ -8,4 +8,16 @@
   	belongs_to :user  
   	validates :title, presence: true, uniqueness: true
   	validates :body, presence: true, length: { minimum: 10 }
+    before_save :set_visits_count
+
+    def update_visits_count
+      self.save if self.visits_count.nil?
+      self.update(visits_count: self.visits_count + 1)
+    end
+
+    private
+
+    def set_visits_count
+      self.visits_count ||= 0
+    end
 end
